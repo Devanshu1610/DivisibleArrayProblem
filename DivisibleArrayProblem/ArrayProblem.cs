739d3dd4dced1bcb710b6f57f7dc9fcce7bf3db4
Devanshu1610 <61723498+Devanshu1610@users.noreply.github.com>
2021-06-28 20:31:03 +0530

    Update ArrayProblem.cs

--- DivisibleArrayProblem/ArrayProblem.cs
+++ DivisibleArrayProblem/ArrayProblem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 
@@ -12,27 +12,37 @@ class DivisibleArray
   
         if (arrayList.Count > 0 && number > 0)
         {
+            //prepare dictionary of divisibilty count and array(s)
             foreach (int [] array in arrayList)
             {
                 int divisiblityCount = 0;
-                for (int i = 0; i < array.Length; i++)
+                try
                 {
-                    if (array[i] % number == 0)
+                    for (int i = 0; i < array.Length; i++)
                     {
-                        divisiblityCount++;
+                        if (array[i] % number == 0)
+                        {
+                            divisiblityCount++;
+
+                        }
 
                     }
-
+                }
+                catch (IndexOutOfRangeException io)
+                {
+                    throw new IndexOutOfRangeException("Error occured in process " + io.Message);
                 }
 
                 if (divisiblityCount > 0)
                 {
+                    //add key divisibilty count and array to the dictionary
                     if (!arraydictionary.ContainsKey(divisiblityCount))
                     {
                         arraydictionary.Add(divisiblityCount, new List<int[]>() { array});
                     }
                     else
                     {
+                        //divisibility count already exists so attach the array 
                         arraydictionary[divisiblityCount].Add(array);                     
                                             
 
@@ -49,31 +59,40 @@ class DivisibleArray
                 
                 if (item.Value.Count > 1)
                 {
-                    //Handle arrays with same number of items divisible by number
-                    //create new array 
-                    int newArrLenght = item.Key * 2;
+                    
+                    int newArrLenght = item.Key * item.Value.Count;
                     int[] newArr = new int[newArrLenght];
                     int k = 0, a;
-                    for (a = 0; a < item.Value.Count; a++)
+                    try
                     {
-
-                        for (int j = 0; j < item.Value[a].Length; j++)
+                        //Handle arrays with same number of items divisible by number
+                        //create new array 
+                        for (a = 0; a < item.Value.Count; a++)
                         {
-                            if ((item.Value[a][j]) % number == 0)
+
+                            for (int j = 0; j < item.Value[a].Length; j++)
                             {
-                                newArr[k] = item.Value[a][j];
-                                k++;
-                                if (k == item.Key)
-                                    break;
+                                if ((item.Value[a][j]) % number == 0)
+                                {
+                                    newArr[k] = item.Value[a][j];
+                                    k++;
+                                    if (k == item.Key)
+                                        break;
+
+                                }
 
                             }
 
                         }
-
                     }
+                    catch (IndexOutOfRangeException io)
+                    {
+                        throw new IndexOutOfRangeException("Error occured in processing " + io.Message);
+
+                    }  
                     return newArr;
-                }
-                
+                    
+                }              
                     
               }
         }   
@@ -111,11 +130,21 @@ class Program
         arraycollection.Add(new int[] { 1, 3, 4, 6, 9 });
         arraycollection.Add(new int[] { 10, 12, 9, 7, 8 });
         arraycollection.Add(new int[] { 12, 6, 9, 18, 14 });
-        var resultarray = DivisibleArray.FindTheMostDivisbleArray(arraycollection, 3);
-        for (int i = 0; i < resultarray.Length; i++)
+        try
         {
-            Console.Write(resultarray[i] + ",");
+            var resultarray1 = DivisibleArray.FindTheMostDivisbleArray(arraycollection, 3);
+            Console.WriteLine("Processing successful!!");
+            for (int i = 0; i < resultarray1.Length; i++)
+            {
+                Console.Write(resultarray1[i] + ",");
+
+            }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Processing failed!! " + ex.Message);
+        }
+
 
         arraycollection.Clear();
         Console.Write('\n');
@@ -125,19 +154,23 @@ class Program
         arraycollection.Add(new int[] { 3, 9, 10, 11, 18 });
         arraycollection.Add(new int[] { 10, 12, 9, 7, 8 });
         arraycollection.Add(new int[] { 12, 7, 9, 8, 14 });
+        arraycollection.Add(new int[] { 30,27, 15, 8, 14 });
 
-        resultarray = DivisibleArray.FindTheMostDivisbleArray(arraycollection, 3);
-        for (int i = 0; i < resultarray.Length; i++)
+        try
         {
-            Console.Write(resultarray[i] + ",");
+
+         var   resultarray2 = DivisibleArray.FindTheMostDivisbleArray(arraycollection, 3);
+            Console.WriteLine("Proccessing successful!!");
+            for (int i = 0; i < resultarray2.Length; i++)
+            {
+                Console.Write(resultarray2[i] + ",");
+            }
+
         }
-
-
-
-
-
-
-
-
+        catch(Exception ex)
+        {
+            Console.WriteLine("Proccessing failed "+ex.Message);
+        }
+      
     }
 }
